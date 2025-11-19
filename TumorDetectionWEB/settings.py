a3e26afe23b6aab910fb3cd83f98b8840b5632b4
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-11-19 11:05:52 -0600

    refactor settings Django proyect

--- TumorDetectionWEB/settings.py
+++ TumorDetectionWEB/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9c6u58r-%18=!^96q^=c0y%n3((*t0*^3rj85&wloxedq1sn=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 load_dotenv()
 
